fd03d4d33607e7ca4441d40a20ad32333ced2d5b
MatteoBrocchi <matteo.brocchi2002@gmail.com>
2020-02-12 10:17:15 +0100

    rr

--- out/main.js
+++ out/main.js
@@ -19,4 +19,28 @@ class Utilita {
         return;
     }
 }
+var codici = new Array();
+class Codici {
+    static getCodice() {
+        if (codici.length != 0) {
+            do {
+                var trovato = false;
+                var random = Math.floor(Math.random() * 5999999);
+                for (let i = 0; i < codici.length; i++) {
+                    if (codici[i] != random)
+                        trovato = true; /*inverto valore solo se trovo diverso*/
+                }
+            } while (!trovato);
+        }
+        else
+            var random = Math.floor(Math.random() * 5999999);
+        codici.push(random);
+        console.log(random);
+        return random;
+    }
+}
+var L1 = new LAT(Codici.getCodice(), "Acqua", 1);
+var B1 = new BID(Codici.getCodice(), "Olio", 5);
+var L2 = new LAT(Codici.getCodice(), "Vino", 2);
+var B2 = new BID(Codici.getCodice(), "Aceto", 4);
 //# sourceMappingURL=main.js.map
