fa425daf7fd6504896d22add9d563b94deff8c54
MatteoBrocchi <matteo.brocchi2002@gmail.com>
2020-02-12 09:53:00 +0100

    ciao

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,30 @@
 abstract class Contenitore {
     protected nome: string;
     protected quantita: number;
-    protected codide:string;
-    constructor(nome: string, quantita: number, codice:string) {
+    protected codide: string;
+    constructor(nome: string, quantita: number, codice: string) {
         this.nome = nome;
         this.quantita = quantita;
     }
 }
-class LAT extends Contenitore{
-    constructor(codice:string,nome:string,quantita:number){
-        super(nome,quantita,codice)
+class LAT extends Contenitore {
+    constructor(codice: string, nome: string, quantita: number) {
+        super(nome, quantita, codice)
     }
 }
 
-class BID extends Contenitore{
-    constructor(codice:string,nome:string,quantita:number){
-        super(nome,quantita,codice)
+class BID extends Contenitore {
+    constructor(codice: string, nome: string, quantita: number) {
+        super(nome, quantita, codice)
     }
 }
 
-class Utilita{
-    public filtraggio(z:Contenitore[],contenitore:Contenitore):Contenitore{
-         return
+class Utilita {
+    public filtraggio(z: Contenitore[], contenitore: Contenitore): Contenitore {
+        return
     }
 }
 
-interface CMP{
+interface CMP {
     compara()
 }
